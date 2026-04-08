683cf3598f4fb5978ad24ffebf60c6c81d3448e8
cierramckay <cierra.mckay@gwmail.gwu.edu>
2026-04-08 15:35:18 -0400

    Cierra

--- teamproj.py
+++ teamproj.py
@@ -1,4 +1,13 @@
+""""
 print("Hello!")
 print("Eden was here!")
 print("Lily was here!")
 print("Chloe was here!!!!!!")
+"""
+import pandas as pd
+from holoviews.ipython import display
+
+file="final_data.csv"
+data=pd.read_csv(file)
+dose_col=data.loc[:,["Reporting_TimeFrame","Cumulative_Flu_Doses_Distributed"]]
+display(dose_col)
